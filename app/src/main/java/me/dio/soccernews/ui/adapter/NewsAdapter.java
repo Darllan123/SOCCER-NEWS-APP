ee9ef85c91f57334cd6c27d59b2e54f3fb26fc0e
Darllan <darllanrbr7@gmail.com>
2022-07-11 15:36:25 -0300

    "Atualizando o App"

--- app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
@@ -3,6 +3,7 @@ package me.dio.soccernews.ui.adapter;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -18,8 +19,11 @@ import me.dio.soccernews.domain.News;
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
 
     private final List<News> news;
-    public NewsAdapter(List<News> news){
+    private final View.OnClickListener favoriteListener ;
+    public NewsAdapter(List<News> news , View.OnClickListener favoriteListener){
+
         this.news =news;
+        this .favoriteListener= favoriteListener;
     }
 
     @NonNull
@@ -34,15 +38,25 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         News news = this.news.get(position);
-        holder.binding.tvTitle.setText(news.getTitle());
-        holder.binding.tvDescription.setText(news.getDescription());
-        Picasso.get().load(news.getImage()).into(holder.binding.ivThumbnail);
+        holder.binding.tvTitle.setText(news.title);
+        holder.binding.tvDescription.setText(news.description);
+        Picasso.get().load(news.image).into(holder.binding.ivThumbnail);
+        // implementaçao da funcionalidade de "abrir link"
         holder.binding.buttonOpenLink.setOnClickListener(view -> {
             Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(news.getLink()));
+            i.setData(Uri.parse(news.link));
             holder.itemView.getContext().startActivity(i);
         });
-
+        // implementaçao da funcionalidade de "compartilhar"
+        holder.binding.ivShare.setOnClickListener(view -> {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(android.content.Intent.EXTRA_SUBJECT, news.title);
+            i.putExtra(Intent.EXTRA_TEXT, news.link);
+            holder.itemView.getContext(). startActivity(Intent.createChooser(i, "Share "));
+        });
+        // implementaçao da funcionalidade de "favoritar"( o evento sera instanciado pelo fragmento
+        holder.binding.ivFavorite.setOnClickListener(  this .favoriteListener);
     }
 
     @Override
@@ -59,6 +73,5 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
     }
 
 }
-
 
 
